2773e1b90606ec3512c2d40fe15082852c6685c3
Vincent <chowzhiho321@gmail.com>
2020-02-08 23:15:08 +0800

    Updates

---
+++ precache-manifest.dc35dbb751343667c7ccd1efe95d064e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48b7f1bacda3c9e8bbb71c5577b13e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1e642c86a1768587db4",
+    "url": "/static/css/main.c36201f3.chunk.css"
+  },
+  {
+    "revision": "acd67ae154e5adadd95a",
+    "url": "/static/js/2.3badebaa.chunk.js"
+  },
+  {
+    "revision": "d1e642c86a1768587db4",
+    "url": "/static/js/main.3891d85d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
